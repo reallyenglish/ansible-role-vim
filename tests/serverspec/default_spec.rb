720e3f4101a1b47f8f890926be0feec3f25a0e33
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-07-21 17:57:50 +0900

    [bugfix] fix the spec
    also, test the binary

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,9 +2,16 @@ require "spec_helper"
 require "serverspec"
 
 package = "vim"
+additional_packages = []
 
 describe package(package) do
   it { should be_installed }
+end
+
+describe command("vim --version") do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/^VIM\s/) }
+  its(:stderr) { should eq "" }
 end
 
 additional_packages.each do |p|
